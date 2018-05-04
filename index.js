17f577601f248b214bc01b581d54cf22515b3621
Joshua Wessels <joshuawessels04@gmail.com>
2018-05-04 13:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joshua"
-var height = 
+var name = "Joe"
+var height = "74"
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
